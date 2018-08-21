26b51fb3d82ee253756da7e0f4f960bda66fb579
Joel Rodriguez <joelerll@gmail.com>
2018-08-21 17:26:15 -0500

    web service

--- client/src/store/mutations.js
+++ client/src/store/mutations.js
@@ -1,6 +1,9 @@
 export default {
-  setUsuario (state, usuario) {
-    state.usuario = usuario
+  setUsuario (state, { codigo, escuela, id, nombre }) {
+    state.usuario = nombre
+    state.codigo = codigo
+    state.escuela = escuela
+    state.id = id
   },
   setLoggeado (state) {
     state.estaLoggeado = true
