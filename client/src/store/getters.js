42babe1a957e5a70693af5614dbddf61c848fee2
Joel Rodriguez <joelerll@gmail.com>
2018-08-21 18:22:57 -0500

    login xml

--- client/src/store/getters.js
+++ client/src/store/getters.js
@@ -1,5 +1,13 @@
 export default {
   estaLoggeado (state) {
     return state.estaLoggeado
+  },
+  obtenerUsuario (state) {
+  	return {
+  	  usuario: state.usuario,
+      codigo: state.codigo,
+	    escuela: state.escuela,
+	    id: state.id
+  	}
   }
 }
