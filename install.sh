6375f68b6406f786656fabf2fcc71d5f5c431598
Joel Rodriguez <joelerll@gmail.com>
2018-09-17 20:08:34 -0500

    cambio

--- install.sh
+++
@@ -1,9 +0,0 @@
-git clone https://github.com/joelerll/web-educate.git
-cd web-educate 
-npm install 
-cd client
-npm install
-npm run build
-cd ..
-
-# NODE_ENV=production forever -o log.log -e err.log app.js 
